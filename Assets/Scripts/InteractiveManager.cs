bca42bedc0b0cf540d4ded2c5e579a958c9ccfaa
hyram2 <hyramfava@gmail.com>
2020-04-14 12:17:35 -0300

    Init
    init

---
+++ Assets/Scripts/InteractiveManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveManager : MonoBehaviour
+{
+	public static InteractiveManager instance;
+
+	public InteractiveTeleport lastTeleport;
+
+	public GameObject player;
+
+	public Light light;
+	public Color neutralColor, badColor, goodColor;
+	// Use this for initialization
+	void Start()
+	{
+		if (instance == null)
+			instance = this;
+	}
+
+	public void Teleport(InteractiveTeleport tp)
+	{
+		if (lastTeleport != null)
+		{
+			lastTeleport.gameObject.SetActive(true);
+			lastTeleport.SetActive(true);
+			lastTeleport.isDisabled = false;
+		}
+
+		light.color = tp.mood == MoodType.Good ? goodColor : tp.mood == MoodType.Bad ? badColor : neutralColor;
+		var tempPos = tp.transform.position;
+		tempPos.y = 1.6f;
+		player.transform.position = tempPos;
+		player.transform.LookAt(transform);
+		lastTeleport = tp;
+	}
+}
---
+++ Assets/Scripts/InteractiveTeleport.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractiveTeleport : MonoBehaviour
+{
+    public Vector3 rotation;
+
+    public GameObject cam;
+
+    public bool isDisabled;
+
+    public GameObject[] disableObjs;
+    public GameObject[] selectedObjs;
+    public GameObject rotObj;
+
+    public GameObject effect;
+    public MoodType mood;
+
+    public void Update()
+    {
+        var tempLookPos = cam.transform.position;
+        tempLookPos.y = 0;
+        rotObj.transform.LookAt(tempLookPos);
+    }
+    
+    public void Enable()
+    {
+        effect.SetActive(true);
+    }
+
+    public void Disable()
+    {
+        effect.SetActive(false);
+    }
+
+    public void Click()
+    {
+        isDisabled = true;
+        InteractiveManager.instance.Teleport(this);
+        print("click");
+        SetActive(false);
+        Disable();
+        rotObj.transform.rotation = new Quaternion(0,0,0,0);
+        gameObject.SetActive(false);
+    }
+
+    public void SetActive(bool active)
+    {
+        foreach (var selectedObj in selectedObjs)
+        {
+            selectedObj.SetActive(!active);
+        }
+
+        foreach (var disableObj in disableObjs)
+        {
+            disableObj.SetActive(active);
+        }
+    }
+
+}
+
+public enum MoodType
+{
+    Good,
+    Bad, 
+    Neutral
+}
